3927af33b02acec12a5e3b7e930181f85c6ad6a9
Pheobe-Apondi <pheobegloriaapondi.com>
2025-03-17 19:48:38 +0300

    did string operations

---
+++ index.js
@@ -0,0 +1,40 @@
+//Extract the last four characters from the string below;"extravaganza"
+let c = 'extravaganza';
+console.log(c.slice (8));
+
+//Insert the following string at the fourth index of the below variable:"eat" const food = "The quick fox jumped over the lazy dog"
+ 
+const food = 'The quick fox jumped over the lazy dog';
+ console.log(food.replace(food[4], 'eat'));
+
+ //Count how many times the following string appears in the string variable:1. "the" 2."brown"const story= "The quick brown fox jumps over the lazy dog"
+ const story = "the quick brown fox jumps over the lazy dog"
+ console.log(story.split('the').length-1);
+ console.log(story.split('brown').length-1); 
+
+ //Using JavaScript, find the following words from the following strings:
+ // 1. "are" const string1 = "The pupils are reading in the library";
+    const string1 =  "The pupils are reading in the library" ;
+    console.log(string1.search('are'));
+
+ // 2. "sitting"const string2 = "The child was sitting on the table before it fell"
+     const string2 = "The child was sitting on the table before it fell"
+     console.log(string2.match('sitting'));
+
+//Convert the following strings into the specified format:
+// 1. UpperCase: "wonderful"
+      let dog = 'wonderful';
+      console.log(dog.toUpperCase());
+
+// 2. LowerCase: "amazing", "UndERneath"
+     let day = 'amazing';
+     console.log(day.toLowerCase());
+
+     let s= 'undERneath' ;
+     console.log(s.toLowerCase());
+// 3. Title case "A wonderful world"
+     let message = 'A wonderful world'
+         message = message.toLowerCase();
+     let  letter = letter.toUpperCase();
+   console.log(message.replace());
+
